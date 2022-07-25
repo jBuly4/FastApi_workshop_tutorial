46e1be8abe107982f8343856ce056a5888cf90e3
jBuly4 <ibuly4@gmail.com>
2022-07-25 16:53:21 +0300

    created methods and classes for auth in services, created models for auth, empty methods for endpoint, updated setting, reqs and .env

--- src/workshop/settings.py
+++ src/workshop/settings.py
@@ -5,10 +5,18 @@ class Settings(BaseSettings):
     server_host: str = '127.0.0.1'
     server_port: int = 8000
     database_url: str = 'sqlite:///./database.sqlite3'
+    # for services/auth.py we need to decode secret and algorithm from payload. so we need this fields
+    jwt_secret: str  # for this field we can use from secrets import token_urlsafe token_urlsafe(32) and save it in
+    # .env file
+    jwt_algorithm: str = 'HS256'
+    jwt_expiration: int = 3600
 
 '''
 when instance of settings is created then that class reads env variables and use their values if their name are the 
-same as class attributes. after that values are checked, validated and process type convertion
+same as class attributes. after that values are checked, validated and process type conversion
+
+for jwt we need python-jose
+for hashing passwords we need passlib[bcrypt]
 '''
 
 settings = Settings(
--- src/workshop/tables.py
+++ src/workshop/tables.py
@@ -6,6 +6,17 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 
+# we need user model to create services with registration and authorization
+# to simplify - we won't create migrations. just recreate db
+class User(Base):
+    __tablename__ = 'users'
+
+    id = sa.Column(sa.Integer, primary_key=True)
+    email = sa.Column(sa.Text, unique=True)
+    username = sa.Column(sa.Text, unique=True)
+    password_hash = sa.Column(sa.Text)
+
+
 class Operation(Base):
     __tablename__ = 'operations'
     id = sa.Column(sa.Integer, primary_key=True)
@@ -14,10 +25,13 @@ class Operation(Base):
     amount = sa.Column(sa.Numeric(10, 2))
     description = sa.Column(sa.String, nullable=True)  # nullable means that this field is optional
 
+    user_id = sa.Column(sa.Integer, sa.ForeignKey('users.id'))
+
+
 '''
 creation of test table through python console
-from workshop.database import engine
-from workshop.tables import Base
+from src.workshop.database import engine
+from src.workshop.tables import Base
 
 Base.metadata.create_all(engine)
 '''
